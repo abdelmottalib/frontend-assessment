62d096cb8b7ecc54163713d295a4101069481e47
konami2 <abdelmottaliblol@gmail.com>
2024-06-14 10:53:17 +0100

    escape closes the modal and replace img tag with Image tag native to nextJs

--- app/components/GifGrid.jsx
+++ app/components/GifGrid.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import Image from "next/image";
 
-const GifGrid = ({gifImages, handleImageClick, related, setSelectedImage}) => {
+const GifGrid = ({
+  gifImages,
+  handleImageClick,
+  related,
+  setSelectedImage,
+}) => {
   return (
     <div className="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {gifImages.map((gif, index) => (
         <div
           key={index}
-          className={`${related ? "w-[100px]" : "w-full"} border border-gray-300 rounded-lg overflow-hidden cursor-pointer flex flex-col`}
+          className={`${
+            related ? "w-[100px]" : "w-full"
+          } border border-gray-300 rounded-lg overflow-hidden cursor-pointer flex flex-col`}
           onClick={() => {
-            related ? setSelectedImage(gifImages[index]):
-            handleImageClick(index)
-        }}
+            related
+              ? setSelectedImage(gifImages[index])
+              : handleImageClick(index);
+          }}
         >
-          <img
+          <Image
             src={gif.images.fixed_width_small.url}
             alt={gif.title}
-            className={"w-full h-full object-cover"}
+            width={gif.images.fixed_width_small.width}
+            height={gif.images.fixed_width_small.height}
+            className="object-cover w-full h-full"
+            unoptimized
           />
         </div>
       ))}
--- app/page.js
+++ app/page.js
@@ -1,161 +1,184 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import GifGrid from "./Components/GifGrid";
 import { debounce } from "lodash";
+import Image from "next/image";
 
 export default function Home() {
-const [gifImages, setGifImages] = useState([]);
-const [searchQuery, setSearchQuery] = useState("");
-const [selectedImage, setSelectedImage] = useState(null);
-const [imageSize, setImageSize] = useState("original");
-const [relatedGifImages, setRelatedGifImages] = useState([]);
+  const [gifImages, setGifImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageSize, setImageSize] = useState("original");
+  const [relatedGifImages, setRelatedGifImages] = useState([]);
 
-const fetchTrendingGIFs = async () => {
-  const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
-  try {
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/trending`, {
-      params: {
-        api_key: apiKey,
-        limit: 25,
-        offset: 0,
-        lang: "en",
-        bundle: "messaging_non_clips",
-      },
-    });
-    const data = response.data.data;
-    const sortedData = data.sort(
-      (a, b) => new Date(a.import_datetime) - new Date(b.import_datetime)
-    );
-    const filteredData = sortedData.filter((gif) => gif.username.length > 0);
-    setGifImages(filteredData);
-  } catch (error) {
-    console.error("Error fetching trending GIFs:", error);
-    setGifImages([]);
-  }
-};
-
-useEffect(() => {
-  fetchTrendingGIFs();
-}, []);
-
-const debouncedSearch = debounce((query) => {
-  if (query.trim() !== "") {
-    fetchData(query, false);
-  } else {
-    setGifImages([]);
-  }
-}, 500);
-
-const handleSearchChange = (e) => {
-  const query = e.target.value;
-  setSearchQuery(query);
-  debouncedSearch(query);
-};
-
-const fetchData = async (query, isRelated) => {
-  const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
-  try {
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/search`, {
-      params: {
-        api_key: apiKey,
-        q: query,
-        limit: isRelated ? 10 : 25,
-        offset: 0,
-        lang: "en",
-        bundle: "messaging_non_clips",
-      },
-    });
-    const data = response.data.data;
-    const sortedData = data.sort(
-      (a, b) => new Date(a.import_datetime) - new Date(b.import_datetime)
-    );
-    const filteredData = sortedData.filter((gif) => gif.username.length > 0);
-    if (isRelated) {
-      setRelatedGifImages(filteredData);
-    } else {
+  const fetchTrendingGIFs = async () => {
+    const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
+    try {
+      const response = await axios.get(
+        `https://api.giphy.com/v1/gifs/trending`,
+        {
+          params: {
+            api_key: apiKey,
+            limit: 25,
+            offset: 0,
+            lang: "en",
+            bundle: "messaging_non_clips",
+          },
+        }
+      );
+      const data = response.data.data;
+      const sortedData = data.sort(
+        (a, b) => new Date(a.import_datetime) - new Date(b.import_datetime)
+      );
+      const filteredData = sortedData.filter((gif) => gif.username.length > 0);
       setGifImages(filteredData);
+    } catch (error) {
+      console.error("Error fetching trending GIFs:", error);
+      setGifImages([]);
     }
-  } catch (error) {
-    console.error("Error fetching GIFs:", error);
-    setGifImages([]);
-  }
-};
+  };
 
-const handleImageClick = (index) => {
-  // console.log(gifImages[index].title);
-  setSelectedImage(gifImages[index]);
-  fetchData(gifImages[index].title.substring(0, 37), true);
-};
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        handleCloseModal();
+      }
+    };
 
-const handleCloseModal = () => {
-  setSelectedImage(null);
-  setRelatedGifImages([]);
-};
+    window.addEventListener("keydown", handleEsc);
 
-const changeImageSize = (size) => {
-  setImageSize(size);
-};
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
 
-return (
-  <div className="flex justify-center">
-    <div className="w-full max-w-4xl px-4">
-      <div className="flex flex-col items-center mt-5">
-        <div className="mb-4">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            placeholder="Search for gifs..."
-            className="p-2 border border-gray-300 rounded-lg focus:outline-none"
-          />
-        </div>
-        <GifGrid
-          gifImages={gifImages}
-          handleImageClick={handleImageClick}
-          related={false}
-        />
-      </div>
-      {selectedImage && (
-        <div
-          className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-75"
-          onClick={handleCloseModal}
-        >
-          <div
-            className="relative flex flex-col items-center"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className="mb-2">
-              <button
-                onClick={() => changeImageSize("original")}
-                className={`p-2 ${imageSize == "original" ? "bg-green-500":"bg-blue-500"} text-white rounded-md mr-2`}
-              >
-                Smaller Size
-              </button>
-              <button
-                onClick={() => changeImageSize("bigger")}
-                className={`p-2 ${imageSize == "bigger" ? "bg-green-500":"bg-blue-500"} text-white rounded-md`}
-              >
-                Bigger Size
-              </button>
-            </div>
-            <img
-              src={selectedImage.images.original.url}
-              alt={selectedImage.title}
-              className={`${
-                imageSize === "bigger" ? "w-[400px]" : "w-[200px]"
-              } max-h-full`}
-            />
-            <GifGrid
-              gifImages={relatedGifImages}
-              handleImageClick={handleImageClick}
-              related={true}
-              setSelectedImage={setSelectedImage}
+  useEffect(() => {
+    fetchTrendingGIFs();
+  }, []);
+
+  const debouncedSearch = debounce((query) => {
+    if (query.trim() !== "") {
+      fetchData(query, false);
+    } else {
+      setGifImages([]);
+    }
+  }, 500);
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    debouncedSearch(query);
+  };
+
+  const fetchData = async (query, isRelated) => {
+    const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
+    try {
+      const response = await axios.get(`https://api.giphy.com/v1/gifs/search`, {
+        params: {
+          api_key: apiKey,
+          q: query,
+          limit: isRelated ? 10 : 25,
+          offset: 0,
+          lang: "en",
+          bundle: "messaging_non_clips",
+        },
+      });
+      const data = response.data.data;
+      const sortedData = data.sort(
+        (a, b) => new Date(a.import_datetime) - new Date(b.import_datetime)
+      );
+      const filteredData = sortedData.filter((gif) => gif.username.length > 0);
+      if (isRelated) {
+        setRelatedGifImages(filteredData);
+      } else {
+        setGifImages(filteredData);
+      }
+    } catch (error) {
+      console.error("Error fetching GIFs:", error);
+      setGifImages([]);
+    }
+  };
+
+  const handleImageClick = (index) => {
+    // console.log(gifImages[index].title);
+    setSelectedImage(gifImages[index]);
+    fetchData(gifImages[index].title.substring(0, 37), true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    setRelatedGifImages([]);
+  };
+
+  const changeImageSize = (size) => {
+    setImageSize(size);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-4xl px-4">
+        <div className="flex flex-col items-center mt-5">
+          <div className="mb-4">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              placeholder="Search for gifs..."
+              className="p-2 border border-gray-300 rounded-lg focus:outline-none"
             />
           </div>
+          <GifGrid
+            gifImages={gifImages}
+            handleImageClick={handleImageClick}
+            related={false}
+          />
         </div>
-      )}
+        {selectedImage && (
+          <div
+            className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-75"
+            onClick={handleCloseModal}
+          >
+            <div
+              className="relative flex flex-col items-center"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="mb-2">
+                <button
+                  onClick={() => changeImageSize("original")}
+                  className={`p-2 ${
+                    imageSize == "original" ? "bg-green-500" : "bg-blue-500"
+                  } text-white rounded-md mr-2`}
+                >
+                  Smaller Size
+                </button>
+                <button
+                  onClick={() => changeImageSize("bigger")}
+                  className={`p-2 ${
+                    imageSize == "bigger" ? "bg-green-500" : "bg-blue-500"
+                  } text-white rounded-md`}
+                >
+                  Bigger Size
+                </button>
+              </div>
+              <Image
+                src={selectedImage.images.original.url}
+                alt={selectedImage.title}
+                width={imageSize === "bigger" ? 400 : 200}
+                height={imageSize === "bigger" ? 400 : 200}
+                className="max-h-full"
+                unoptimized={true}
+              />
+              <GifGrid
+                gifImages={relatedGifImages}
+                handleImageClick={handleImageClick}
+                related={true}
+                setSelectedImage={setSelectedImage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
 }
